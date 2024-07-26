50dcc46598125357b336556eb327a42b97a5a385
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-07-26 02:22:45 +0000

    task2:A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -14,22 +14,22 @@ set name(name) {
     this._name = name;
   }
 
- get length() {
+get length() {
     return this._length;
   }
 
-  set length(length) {
+set length(length) {
     if ((typeof length !== 'number') && !(length instanceof Number)) {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  get students() {
+get students() {
     return this._students;
   }
 
-  set students(students) {
+set students(students) {
     if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
